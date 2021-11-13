d61b2d2f6590375ac368ea4cefccc29eaeb4a73a
Arthur Opriatnyi <imarrche@gmail.com>
2021-11-13 18:43:51 +0200

    Add HTTP server

--- cmd/urx/main.go
+++ cmd/urx/main.go
@@ -1,7 +1,46 @@
 package main
 
-import "fmt"
+import (
+	"context"
+	"fmt"
+	"log"
+	"net/http"
+	"os"
+	"os/signal"
+	"time"
+
+	"go.uber.org/zap"
+)
 
 func main() {
-	fmt.Println("urx")
+	logger, err := zap.NewProduction()
+	if err != nil {
+		log.Fatal(err)
+	}
+	undo := zap.ReplaceGlobals(logger)
+	defer undo()
+
+	http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
+		_, _ = fmt.Fprint(w, "urx")
+	})
+	s := &http.Server{
+		Addr: ":8080",
+	}
+	go func() {
+		if err := s.ListenAndServe(); err != nil && err != http.ErrServerClosed {
+			zap.L().Fatal("couldn't start HTTP server")
+		}
+	}()
+	zap.L().Info("starting HTTP server on :8080")
+
+	done := make(chan os.Signal, 1)
+	signal.Notify(done, os.Interrupt)
+	<-done
+
+	ctx, cancel := context.WithTimeout(context.Background(), 5*time.Second)
+	defer cancel()
+	if err := s.Shutdown(ctx); err != nil {
+		zap.L().Fatal("HTTP server couldn't shut down gracefully")
+	}
+	zap.L().Info("HTTP server shut down gracefully")
 }
--- go.mod
+++ go.mod
@@ -1,3 +1,5 @@
 module urx
 
 go 1.16
+
+require go.uber.org/zap v1.19.1
